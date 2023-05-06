00b2ebf6117643981d3a02c63fdad6f5605bd6df
Dirk Groeneveld <dirkg@allenai.org>
2023-05-06 03:39:44 +0300

    Print less

--- run_with_environment.sh
+++ run_with_environment.sh
@@ -14,9 +14,13 @@ export LOCAL_WORLD_SIZE=$SLURM_NTASKS_PER_NODE
 export LOCAL_RANK=$SLURM_LOCALID
 export NODE_RANK=$((($RANK - $LOCAL_RANK) / $LOCAL_WORLD_SIZE))
 
-rm -f /dev/shm/rocm_smi*
-
 exec > >(trap "" INT TERM; sed -u "s/^/$NODENAME:$LOCAL_RANK out: /")
 exec 2> >(trap "" INT TERM; sed -u "s/^/$NODENAME:$LOCAL_RANK err: /" >&2)
+
+set -xeuo pipefail
+
+rm -f /dev/shm/rocm_smi*
+
+sleep 5
 
 exec $*
